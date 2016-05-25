44c4acfcf50bbd7dfc20320f5400d6321e859c74
Randy Marmer <rmarmer1@yahoo.com>
2016-05-25 17:26:03 +0100

    Raising our 1st error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,19 @@ require_relative 'bike'
 
 class DockingStation
 
+
 	attr_reader :bike
 
-	def release_bike 
-		Bike.new
-	end
+# in this case, @bike is actually 'nil' - which EVALUATES to 'false' (falsey)
+def release_bike 
+	fail 'No bikes available' unless @bike
+	@bike
+end
 
-	def dock(bike)
-		@bike = bike
-	end
+def dock(bike)
+	@bike = bike
 
-	
+end
+
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,10 +6,10 @@ describe DockingStation do
 		expect(subject).to respond_to :release_bike
 	end
 
-	it "returns a working bike" do
-		bike = subject.release_bike
-		expect(bike).to be_working
-	end
+	# it "returns a working bike" do
+	# 	bike = subject.release_bike
+	# 	expect(bike).to be_working
+	# end
 
 	it "responds to dock" do
 		expect(subject).to respond_to(:dock).with(1).argument
@@ -29,4 +29,20 @@ describe DockingStation do
 		subject.dock(bike)
 		expect(subject.bike).to eq bike
 	end
+	
+	it "releases a previously docked bike" do
+		bike_inst = Bike.new
+		subject.dock(bike_inst)
+		expect(subject.release_bike).to eq bike_inst
+	end
+
+	describe '#release_bike' do
+		it 'raises an error when there are no bikes available' do
+			expect { subject.release_bike }.to raise_error 'No bikes available'
+		end
+	end
 end	
+
+# As a member of the public,
+# So that I am not confused and charged unnecessarily,
+# I'd like docking stations not to release bikes when there are none available.
